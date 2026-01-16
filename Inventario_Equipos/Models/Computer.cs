697354c3bfc784f1e9384c644769e77eddf46995
Daniel Sulaiman <daniel21sulaiman@gmail.com>
2026-01-16 14:42:22 -0500

    Actualización de modelos principales
    Se crean modelo para tablas de Usuarios y computadores.

--- Inventario_Equipos/Models/Computer.cs
+++ Inventario_Equipos/Models/Computer.cs
@@ -1,6 +1,25 @@
-﻿namespace Inventario_Equipos.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventario_Equipos.Models
 {
     public class Computer
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Serial { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string BrandName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Model { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string OperatingSystem { get; set; }
+        [Required]
+        public bool isFunctional { get; set; }
     }
 }
--- Inventario_Equipos/Models/User.cs
+++ Inventario_Equipos/Models/User.cs
@@ -1,6 +1,24 @@
-﻿namespace Inventario_Equipos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventario_Equipos.Models
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Area { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string JobPosition { get; set; }
+        [MaxLength(3)]
+        public string? Ext { get; set; }
+        [Required]
+        [MaxLength(125)]
+        public string Email { get; set; }
     }
 }
